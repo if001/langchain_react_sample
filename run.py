f03f5b0894693cb6541d337d61406fbe35b4e5d0
windows_on_wsl <otomijuf.004@gmail.com>
2023-05-04 21:09:56 +0900

    fix

--- run.py
+++ run.py
@@ -27,8 +27,6 @@ react_prompt = f"""以下のフォーマットで出力して下さい
 - ツールの選択: {tools}から行動を１つ選択して下さい
 - ツールへの入力: ツールに入力するキーワード"""
 
-# - 観察: 行動により得られた結果から質問の回答に必要な情報を抽出して下さい"""
-
 react_prompt_final_answer = f"""{react_prompt}
 (思考を繰り返し、ツールを選択する必要がない場合、最終的な回答を出力してください。)
 - 最終的な回答: 質問に対する最終的な答えを出力して下さい。"""
@@ -58,12 +56,11 @@ def create_wiki_react_prompt(qa, info):
 ### ASSISTANT: """
 
 
-def create_qa_prompt(qa, info, history):
+def create_qa_prompt(qa, info):
     return f"""### Human: 関連情報を用いて、質問に答えてください。
 
 関連情報: {info}
 
-{history}
 ### HUMAN: {qa}
 ### ASSISTANT: """
 
@@ -116,107 +113,48 @@ def get_final_answer(t):
         return result
     return None
 
-def to_hisotry(qa, final_result):
-    final_result = final_result[:150]
 
-    return f"""
-### HUMAN: {qa}
-### ASSISTANT: {final_result}    
-"""
-
-def check_output(qa):
-    prompt = create_first_prompt(qa)
-
-    print('prompt: ', prompt)
-    output = llm(prompt)
-    print('output: ', output)
-    exit(0)
-
-def check_summary():
-    info = """この項目では、日本の内閣総理大臣の一覧について説明しています。. その他の用法については「 内閣総理大臣 (曖昧さ回避) 」をご覧ください。. 内閣総 理大臣. 現職. 岸田文雄. 第2次岸田改造内閣. 就任日：2021年11月10日. 歴代の首相と内閣. 歴代内閣総理大臣. 内閣総理大臣の一覧 （ないかくそうりだいじんのいちらん）は、 日本 の 行政府の長 である 内閣総理大臣 を務めた人物の一覧である。. 脚注. [ 続きの解説] 「内閣総理大臣の一覧」の続きの解説一覧. 1 内閣総理大臣の一覧とは. 2 内閣総理大臣の一覧の ... この項目では、日本の内閣総理大臣の一覧について説明しています。. その他の用法については「 内閣総理大臣 (曖昧さ回避) 」をご覧ください。. 内閣総理大臣. 現職. 岸田文雄. 第2次岸田改造内閣. 就任日：2021年11月10日. 歴代の首相と内閣. 歴代内閣総理大臣. 内閣総理大臣 （ないかくそうりだいじん、 英: Prime Minister [1] ）は、 日本 の 内閣 の 首長 たる 国務大臣 [2] 。. 文民 である 国会議員 が就任し、その 地位 及び 権限 は 日本国憲法 や 内閣法 などに規定されている [3] 。. 脚注. [ 続きの解説] 「内閣総理大臣 ... 内閣総理大臣 （ないかくそうりだいじん、 英: Prime Minister [1] ）は、 日本 の 内 閣 の 首長 たる 国務大臣 [2] 。. 文民 である 国会議員 が就任し、その 地位 及び 権限 は 日本国憲法 や 内閣法 などに規定されている [3] 。. 脚注. [ 続きの解 説] 「内閣総理大臣"""
-    print(len(info))
-    info = info[500:]
-    p = create_summary_prompt(info)
-    r = llm(p)
-    print('r', r)
-    exit(0)
-
-# qa='埼玉県で一番高い山を教えてください' 
 qa='日本の現在の総理大臣を教えてください' 
-#qa='kubernetesのpodについて教えてください' 
-# qa='kubernetesのserviceとpodの関係について教えてください' 
-# qa='LLMのReActについて教えてください' 
 
-# check_output(qa)
-# check_summary()
 
-while False:
-    while True:
-        prompt = create_first_prompt(qa)
-        print('prompt', prompt)
-        output = llm(prompt)
-        print('first result: ', output)
-        print("==========")
-        output = output + '\n'
+## ツールの選択/ツールへの入力
+while True:
+    prompt = create_first_prompt(qa)
+    output = llm(prompt)
+    output = output + '\n'
 
-        action, action_input = get_tool(output)
-        
-        print('action:', action)
-        print('actoin_input:', action_input)
-        print("==========")
-        if action and action_input:
-            break
-
-    tool_result = None
-    if action == 'Web検索':
-        tool_result = dd_search.run(action_input.strip())
-        tool_result.strip()
-    elif action == 'Wikipedia検索':
-        tool_result = docstore.search(action_input.strip())
-        tool_result.strip()
-    else:
-        tool_result = None
+    action, action_input = get_tool(output)
 
     if tool_result != None and tool_result != '':
-        tool_result = tool_result[:250]
-        print('tool_result', tool_result)
-        print('--------------------')
-        # tool_prompt = create_summary_prompt(tool_result[500:])
-        # tool_result = llm(tool_prompt)
-        # print('tool_result summary', tool_result)
-        # print('--------------------')
+        tool_result = None
+        if action == 'Web検索':
+            tool_result = dd_search.run(action_input.strip())
+            tool_result.strip()
+        elif action == 'Wikipedia検索':
+            tool_result = docstore.search(action_input.strip())
+            tool_result.strip()
+        else:
+            tool_result = None
 
-        prompt = create_wiki_react_prompt(qa, tool_result)
-        print('react prompt', prompt)
-        print('--------------------')
-        while True:
-            output = llm(prompt)
-            print('react result: ', output)
-            print('--------------------')
-            output = output + '\n'
-            print("==========")
-            final_answer = get_final_answer(output)
-            if final_answer:
-                break
+        if tool_result != None and tool_result != '':
+            tool_result = tool_result[:250]
+            break
 
-# pre_qa = "kubernetesのpodについて教えてください"
-# pre_ans = """こんにちは！KubernetesのPODについて説明します。
+            ## summary
+            # tool_prompt = create_summary_prompt(tool_result[500:])
+            # tool_result = llm(tool_prompt)
+            # print('tool_result summary', tool_result)
+            # print('--------------------')
 
-# Kubernetesでは、1つのクラスター内の複数のノードに、1つのポッドを配置することができます。このポッドは、コンテナー内で実行されるアプリケーションやプログラムをカスタマイズしたものになります。
+prompt = create_wiki_react_prompt(qa, tool_result)
+while True:
+    output = llm(prompt)
+    output = output + '\n'
+    final_answer = get_final_answer(output)
+    if final_answer:
+        break
 
-# 各ポッドは、Kubernetesクラスター内の1つのノードに分けられます。このようにすることで、ポッドが互いに独立して実行されるため、1つのアプリケーションの失効や1つのノー ドの挫折による制限を回避することができます。
-
-# また、ポッドは簡単に動的なイ"""
-# history = to_hisotry(pre_qa, pre_ans)
-
-#final_answer = '現在の内閣総理大臣は岸田文雄です。'
-#qa_prompt = create_qa_prompt(qa, final_answer, "")
-
-qa_prompt="""### HUMAN: 日本の現在の総理大臣を教えてください
-### ASSISTANT:
-"""
-print('qa prompt:', qa_prompt)
-print('--------------')
+qa_prompt = create_qa_prompt(qa, final_answer)
 result = llm(qa_prompt)
 print('result: ', result)
 
